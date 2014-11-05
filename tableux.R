bda77ef39e7ec0f0871cfcbfc6d4217a04d2241d
Frank Murphy Hernandez <murphy@ciencias.unam.mx>
2014-11-05 11:53:23 -0600

    Create tableux.R

---
+++ tableux.R
@@ -0,0 +1,11 @@
+tableux<-function(T){
+	UN<-unique(T)
+	RES<-c()
+	for(i in 1:length(T)){
+		RES<-rbind(RES,as.numeric(UN==T[i]))
+		}
+	for(i in 1:ncol(RES)){
+		RES[,i]<-cumsum(RES[,i])
+		}
+	return(RES)
+	}
