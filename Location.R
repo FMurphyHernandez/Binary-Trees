2931b473d4bd7d162185497c7522ac4fb85e3e95
Frank Murphy Hernandez <murphy@ciencias.unam.mx>
2014-11-05 10:34:29 -0600

    Update Location.R

--- Location.R
+++ Location.R
@@ -1,9 +1,13 @@
-loc<-funtion(x){
+loc<-function(x){
+if(x==1)
+return(1)
 n<-ceiling(log(abs(x)+1,2))-1
 E=x
+if(x>3){
 for(i in (n-1):1){
-  x<-2*ceiling(x/2-(2^i))
-  E<-c(x,E)
-  }
-return(E)
+x<-(2^i-1)+ceiling(x/2-(2^i-.5))
+E<-c(x,E)
+}
+}
+return(c(1,E))
 }
